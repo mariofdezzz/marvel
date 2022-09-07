9079dbaa04164eb3d51474fbf8b53a9057807f12
Mario Ferrero <47794126+mariofdezzz@users.noreply.github.com>
2022-09-07 19:16:41 +0100

    feat: add axios to MarvelApi
    remove fetch
    increase readability

--- src/apis/marvel/index.ts
+++ src/apis/marvel/index.ts
@@ -1,41 +1,30 @@
-import { toURLSearchParams } from '@/utils/toURLSearchParams'
+import axios from 'axios'
 import type { MarvelApiResponse } from './types/api'
 import type { Comic } from './types/comics'
-import { OrderBy, type GetComicsParams } from './types/comics/methods'
+import type { GetComicsParams } from './types/comics/methods'
 
-const baseUrl = import.meta.env.VITE_MARVEL_BASE_URL
+const { get } = axios.create({
+  baseURL: import.meta.env.VITE_MARVEL_BASE_URL,
+  params: {
+    apikey: import.meta.env.VITE_MARVEL_API_KEY,
+  },
+})
 
 export class MarvelApi {
   constructor(private apikey: string) {}
 
   comics = {
     findAll: async (params: GetComicsParams = {}): Promise<Comic[]> => {
-      const { orderBy = OrderBy.Asc.onSaleDate } = params
-
-      const searchParams = new URLSearchParams(
-        toURLSearchParams({
-          ...params,
-          apikey: this.apikey,
-          orderBy,
-        })
-      )
-
-      const response: MarvelApiResponse<Comic[]> = await fetch(
-        baseUrl + '/comics?' + searchParams.toString()
-      ).then((res) => res.json())
+      const response: MarvelApiResponse<Comic[]> = await get('/comics', {
+        params,
+      }).then((res) => res.data)
 
       return response.data.results
     },
     find: async (id: number): Promise<Comic> => {
-      const searchParams = new URLSearchParams(
-        toURLSearchParams({
-          apikey: this.apikey,
-        })
-      )
-
-      const response: MarvelApiResponse<Comic[]> = await fetch(
-        baseUrl + `/comics/${id}?` + searchParams.toString()
-      ).then((res) => res.json())
+      const response: MarvelApiResponse<Comic[]> = await get(
+        `/comics/${id}`
+      ).then((res) => res.data)
 
       return response.data.results[0]
     },
